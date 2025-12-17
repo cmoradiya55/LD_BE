8d252034e029d3f51aa3653d313de8b3d29b104c
Ishwar <ishwartrada15@gmail.com>
2025-12-17 09:36:43 +0530

    fix: move email transformation to the email field in UpdateProfileDto

--- src/modules/customer/profile/dto/update-profile.dto.ts
+++ src/modules/customer/profile/dto/update-profile.dto.ts
@@ -4,12 +4,12 @@ import { Transform, Type } from 'class-transformer';
 import { IsEmail, IsNotEmpty, IsOptional, IsString, MaxLength } from 'class-validator';
 
 export class UpdateProfileDto {
-    @Transform(({ value }) => value?.toLowerCase())
     @IsNotEmpty({ message: 'Full name is required' })
     @IsString()
     @MaxLength(255)
     fullName: string;
 
+    @Transform(({ value }) => value?.toLowerCase())
     @IsEmail({}, { message: 'Please provide a valid email address' })
     email: string;
 
