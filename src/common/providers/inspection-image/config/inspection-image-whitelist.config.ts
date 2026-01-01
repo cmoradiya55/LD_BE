9c052a538e5cf66294a54c6b9c2747db128e1c9e
Ishwar <ishwartrada15@gmail.com>
2026-01-01 12:13:13 +0530

    feat(inspection-image): enhance inspection image types and subtypes for better categorization

--- src/common/providers/inspection-image/config/inspection-image-whitelist.config.ts
+++ src/common/providers/inspection-image/config/inspection-image-whitelist.config.ts
@@ -2,27 +2,39 @@
 // config/inspection-image-whitelist.config.ts
 // =============================================
 
-import { InspectionImageType as T, InspectionImageSubtype as S } from '../enum/inspection-image.enum';
+import { InspectionImageType as T, InspectionImageSubType as S } from '../enum/inspection-image.enum';
 
 // [imageType, imageSubtype | null]
 type WhitelistTuple = [number, number | null];
 
 const WHITELIST: WhitelistTuple[] = [
-    [T.EXTERIOR, S.EXTERIOR_FRONT],
-    [T.EXTERIOR, S.EXTERIOR_REAR],
-    [T.EXTERIOR, S.EXTERIOR_LEFT],
-    [T.EXTERIOR, S.EXTERIOR_RIGHT],
-    [T.INTERIOR, S.INTERIOR_DASHBOARD],
-    [T.INTERIOR, S.INTERIOR_FRONT_SEATS],
-    [T.INTERIOR, S.INTERIOR_REAR_SEATS],
-    [T.INTERIOR, S.INTERIOR_BOOT_SPACE],
-    [T.ENGINE, S.ENGINE_BAY,],
-    [T.TYRES, S.TYRE_FRONT_LEFT,],
-    [T.TYRES, S.TYRE_FRONT_RIGHT,],
-    [T.TYRES, S.TYRE_REAR_LEFT,],
-    [T.TYRES, S.TYRE_REAR_RIGHT],
-    [T.TEST_DRIVE, S.ODOMETER],
-    [T.DOCUMENTS, S.DOC_RC_FRONT],
+    [T.EXTERIOR, S[T.EXTERIOR].ROOF],
+
+    [T.EXTERIOR, S[T.EXTERIOR].LEFT_FRONT],
+    [T.EXTERIOR, S[T.EXTERIOR].FRONT],
+    [T.EXTERIOR, S[T.EXTERIOR].LEFT_SIDE],
+    [T.EXTERIOR, S[T.EXTERIOR].LEFT_BACK],
+
+    [T.EXTERIOR, S[T.EXTERIOR].BACK],
+    [T.EXTERIOR, S[T.EXTERIOR].RIGHT_BACK],
+    [T.EXTERIOR, S[T.EXTERIOR].RIGHT_SIDE],
+    [T.EXTERIOR, S[T.EXTERIOR].RIGHT_FRONT],
+
+    [T.EXTERIOR, S[T.EXTERIOR].BOOT_DOOR],
+    [T.EXTERIOR, S[T.EXTERIOR].WINDSHIELD_REAR],
+
+    [T.EXTERIOR, S[T.ENGINE_AND_TRANSMISSION].ENGINE],
+
+    [T.INTERIOR, S[T.INTERIOR].DASHBOARD],
+    [T.INTERIOR, S[T.INTERIOR].ODOMETER],
+    [T.INTERIOR, S[T.INTERIOR].FRONT_SEAT_SIDE],
+    [T.INTERIOR, S[T.INTERIOR].REAR_SEAT_SIDE],
+    [T.INTERIOR, S[T.INTERIOR].BOOT_SPACE],
+    
+    [T.TYRES, S[T.TYRES].FRONT_LEFT],
+    [T.TYRES, S[T.TYRES].FRONT_RIGHT],
+    [T.TYRES, S[T.TYRES].REAR_LEFT],
+    [T.TYRES, S[T.TYRES].REAR_RIGHT],
 ];
 
 export const getWhitelist = () => WHITELIST.map(([type, subtype]) => ({ type, subtype }));
