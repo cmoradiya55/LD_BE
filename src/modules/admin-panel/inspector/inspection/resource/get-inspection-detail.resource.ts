b612ca9101651f9cb6e8b00343c207ad808465b2
Ishwar <ishwartrada15@gmail.com>
2026-01-07 18:25:34 +0530

    feat(inspection): standardize JSON keys in InspectionImageResource and GetInspectionDetailResource

--- src/modules/admin-panel/inspector/inspection/resource/get-inspection-detail.resource.ts
+++ src/modules/admin-panel/inspector/inspection/resource/get-inspection-detail.resource.ts
@@ -9,17 +9,17 @@ export class InspectionImageResource extends BaseResource<any> {
         const data = {
             id: CommonHelper.number(this.data.id),
             type: CommonHelper.number(this.data.image_type),
-            subType: CommonHelper.number(this.data.image_subtype),
-            imageUrl: CommonHelper.buildImageUrl(this.data.image_url),
+            sub_type: CommonHelper.number(this.data.image_subtype),
+            image_url: CommonHelper.buildImageUrl(this.data.image_url),
 
             title: CommonHelper.text(this.data.title),
 
-            isDamage: CommonHelper.bool(this.data.has_damage),
+            is_damage: CommonHelper.bool(this.data.has_damage),
             remarks: CommonHelper.text(this.data.remarks)
         };
 
         if (this.data.image_type === InspectionImageType.TYRES) {
-            data['treadDepth'] = CommonHelper.number(this.data.tread_depth);
+            data['tread_depth'] = CommonHelper.number(this.data.tread_depth);
         }
 
         if (
@@ -30,7 +30,7 @@ export class InspectionImageResource extends BaseResource<any> {
                 this.data.image_subtype === InspectionImageSubType[InspectionImageType.ELECTRICAL].RHS_FRONT_WINDOW ||
                 this.data.image_subtype === InspectionImageSubType[InspectionImageType.ELECTRICAL].RHS_REAR_WINDOW)
         ) {
-            data['isPower'] = CommonHelper.bool(this.data.is_power);
+            data['is_power'] = CommonHelper.bool(this.data.is_power);
         }
 
         return data;
@@ -55,6 +55,7 @@ export class GetInspectionDetailResource extends BaseResource<any> {
 
             rc_image: CommonHelper.buildImageUrl(this.data.rc_image),
             insurance_image: CommonHelper.buildImageUrl(this.data.insurance_image),
+            km_driven: CommonHelper.number(this.data.km_driven),
 
             car: {
                 brand: CommonHelper.text(brand?.display_name),
@@ -65,8 +66,8 @@ export class GetInspectionDetailResource extends BaseResource<any> {
                 fuelType: CommonHelper.number(variant?.fuel_type),
                 fuelTypeLabel: CommonHelper.text(FuelTypeLabel[variant?.fuel_type]),
 
-                registrationYear: CommonHelper.number(this.data.registration_year),
-                registrationNumber: CommonHelper.text(this.data.registration_number),
+                registartion_year: CommonHelper.number(this.data.registration_year),
+                registration_number: CommonHelper.text(this.data.registration_number),
                 ownerType: CommonHelper.number(this.data.owner_type),
                 kmDrivenRange: CommonHelper.number(this.data.km_driven_range),
             },
@@ -82,9 +83,6 @@ export class GetInspectionDetailResource extends BaseResource<any> {
                 state: CommonHelper.capitalizeWords(city?.state_name),
             },
 
-            inspection: {
-                kmDriven: CommonHelper.number(this.data.km_driven),
-            },
             inspectionImages: InspectionImageResource.collection(inspectionImages),
         };
     }
